745768ddc74c31bf856661d1b8eb7f426ed4a77b
Erik Luebbers <erluebbers@gmail.com>
2021-12-11 08:25:10 -0800

    format html body to include ul item

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-console.log("Javascript File is happening")
 document.addEventListener("DOMContentLoaded", () => {
   getBooks()
 
 
 })
 
+function getBooks () {
+  fetch("https://gutendex.com/books?search=austen%20jane", fetchParameters)
+    .then(response => response.json())
+    .then(data => console.log(data.results))
+}
+
 const fetchParameters = {
   method: "GET",
   headers:{
     'Content-Type': 'application/json'
   },
-}
-
-function getBooks () {
-  fetch("https://gutendex.com/books", fetchParameters)
-    .then(response => response.json())
-    .then(data => console.log(data))
 }
