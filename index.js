23a6ef95d4849576a44ddcda6045770c45bf2be5
Erik Luebbers <erluebbers@gmail.com>
2021-12-13 06:02:48 -0800

    fix author sublist

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function createBookList (book) {
   bookItem.addEventListener('click', () => {
     let list = document.createElement("ul")
     list.innerHTML = `
-    <li>Author: ${book.authors[0].name}</li>
+    <li>Author: ${book.authors.includes('obj') ? book.authors[0].name : 'None Listed'}</li>
     <li id="${book.id}">Subjects: </li>
     `
     bookItem.appendChild(list)
