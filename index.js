9ba62ce09d94d80dd40c802c655ee61d3d02b351
Erik Luebbers <erluebbers@gmail.com>
2021-12-17 08:08:49 -0800

    clean up after presentation feedback

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("travel_input").addEventListener('submit', getBooks)
-
 })
 
 function getBooks (e) {
   e.preventDefault()
   const place = (e.target.place.value)
-  fetch(`https://gutendex.com/books?search=${place}%20`, fetchParameters)
+  fetch(`https://gutendex.com/books?search=${place}%20`)
     .then(response => response.json())
-    .then(data => {
-      data.results.forEach(book => createBookList(book))
-      console.log(data.results)
+    .then(({results}) => {
+      results.forEach(book => createBookList(book))
     })
   document.getElementById("travel_input").reset()
 }
 
-const fetchParameters = {
-  method: "GET",
-  headers:{
-    'Content-Type': 'application/json'
-  },
-}
-
 function createBookList (book) {
-  let bookItem = document.createElement('li')
+  const bookItem = document.createElement('li')
   bookItem.innerText = book.title
   document.getElementById("book_container").appendChild(bookItem)
   bookItem.addEventListener('click', () => {
@@ -38,7 +29,7 @@ function createBookList (book) {
 }
 
 function subjectList (array) {
-  let subList = document.createElement('ul')
+  const subList = document.createElement('ul')
   array.forEach(subject => {
     let item = document.createElement('li')
     item.innerText = subject
